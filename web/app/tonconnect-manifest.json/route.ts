eb684eaea1a94946dbe99bdd0d00580575f347a0
chixx <lucottejeremie@gmail.com>
2024-10-18 19:55:39 +0200

    update tonconnect manifest without message

--- web/app/tonconnect-manifest.json/route.ts
+++ web/app/tonconnect-manifest.json/route.ts
@@ -3,11 +3,9 @@ import { NextResponse } from "next/server";
 export async function GET() {
   return NextResponse.json(
     {
-      message: {
-        url: "https://toner-jet.vercel.app/",
-        name: "TONER",
-        iconUrl: "https://ton.vote/logo.png",
-      },
+      url: "https://toner-jet.vercel.app/",
+      name: "TONER",
+      iconUrl: "https://ton.vote/logo.png",
     },
     { status: 500 }
   );
